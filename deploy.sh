9520b169546e6c440374f0f6236f19a8e01669cd
janpf <janpf@users.noreply.github.com>
2023-07-06 14:40:04 +0200

    ratarmount might support nfs

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #kubectl get nodes | grep -v kube | grep -v NAME | awk '{print $1 ".informatik.uni-wuerzburg.de";}' | xargs -I {} ssh {} "/usr/bin/env python3 -m pip install --upgrade ratarmount"
 #kubectl get nodes | grep -v kube | grep -v NAME | awk '{print $1 ".informatik.uni-wuerzburg.de";}' | xargs -I {} ssh {} "mkdir -p /usr/libexec/kubernetes/kubelet-plugins/volume/exec/krause\~fusezip-driver/"
-kubectl get nodes | grep -v kube | grep -v NAME | awk '{print $1 ".informatik.uni-wuerzburg.de";}' | xargs -I {} scp fusezip-driver {}:/usr/libexec/kubernetes/kubelet-plugins/volume/exec/krause\~fusezip-driver/fusezip-driver
+#kubectl get nodes | grep -v kube | grep -v NAME | awk '{print $1 ".informatik.uni-wuerzburg.de";}' | xargs -I {} scp fusezip-driver {}:/usr/libexec/kubernetes/kubelet-plugins/volume/exec/krause\~fusezip-driver/fusezip-driver
 kubectl get nodes | grep -v kube | grep -v NAME | awk '{print $1 ".informatik.uni-wuerzburg.de";}' | xargs -I {} scp ratarmount-driver {}:/usr/libexec/kubernetes/kubelet-plugins/volume/exec/janpf\~ratarmount-driver/ratarmount-driver
